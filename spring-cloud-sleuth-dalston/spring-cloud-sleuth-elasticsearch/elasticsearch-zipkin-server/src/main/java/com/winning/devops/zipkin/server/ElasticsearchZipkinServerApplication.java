a519d65e3392eb0caf1c0a64537973723002a91d
chensj.dev <chenshijie1988@yeah.net>
2019-06-11 21:44:43 +0800

    spring cloud sleuth zipkin elasticsearch

---
+++ spring-cloud-sleuth-dalston/spring-cloud-sleuth-elasticsearch/elasticsearch-zipkin-server/src/main/java/com/winning/devops/zipkin/server/ElasticsearchZipkinServerApplication.java
@@ -0,0 +1,22 @@
+package com.winning.devops.zipkin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.EnableZipkinServer;
+
+/**
+ * 链路数据追踪服务端，记录链路数据
+ * @author chensj
+ * @date 2019年06月06日 10:24
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ElasticsearchZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchZipkinServerApplication.class,args);
+    }
+
+}
